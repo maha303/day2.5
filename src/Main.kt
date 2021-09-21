c7b2eee4a1a1514b43a5b603e0e150cddda679c9
maha303 <86849690+maha303@users.noreply.github.com>
2021-09-21 16:19:16 +0300

    Update Main.kt

--- src/Main.kt
+++ src/Main.kt
@@ -1,31 +1,18 @@
 import kotlin.collections.List as List1
 
 fun main (){
-    var usa = arrayListOf("usa","")
-    var uk = arrayListOf("uk","")
-    var ksa = arrayListOf("ksa","")
+    val countries = listOf("USA","JAPAN","USE")
+
+    val capital = arrayListOf<String>()
+
+    for(i in countries){
+        println("enter the Capital  of $i :")
+        capital.add(readLine()!!)
+    }
 
 
-    println("enter the capital  of USA")
-    var userinput1 = readLine()!!.toString()
-  
-    usa.add(userinput1)
-
-    println("enter the capital  of UK")
-    var userinput2 = readLine()!!.toString()
-   uk.add(userinput2)
-
-    println("enter the capital  of KSA")
-    var userinput3 = readLine()!!.toString()
-  ksa.add(userinput3)
-
-    val a = ArrayList<ArrayList<String>>()
-    a.add(ArrayList(usa))
-    a.add(ArrayList(uk))
-    a.add(ArrayList(ksa))
-
-    for (i in a ){
-        println(i)
+    for(i in 0..2){
+        println("The capital of ${countries[i]} is ${capital[i]}.")
     }
 
 }
